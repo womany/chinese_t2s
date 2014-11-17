941b16d3beb8ae89ccbb67f7f5e4ea3350305cb2
wildjcrt <wildjcrt@yahoo.com.tw>
2014-11-18 06:23:55 +0800

    lib/chinese_t2s, create #get_words and #get_words, and save them in CONSTANT

--- lib/chinese_t2s.rb
+++ lib/chinese_t2s.rb
@@ -1,5 +1,33 @@
 require "chinese_t2s/version"
 
 module ChineseT2s
-  # Your code goes here...
+  private
+  def self.get_words
+    cht_array = []
+    hash = {}
+
+    File.read(File.dirname(__FILE__) + '/../vendor/word_t2s.txt').split("\r\n").each do |mapping|
+      t, s = mapping.split(',')
+      cht_array << t
+      hash[t] = s
+    end
+
+    [cht_array.join('|'), hash]
+  end
+
+  def self.get_phases
+    cht_array = []
+    hash = {}
+
+    File.read(File.dirname(__FILE__) + '/../vendor/phrase_t2s.txt').split("\r\n").each do |mapping|
+      s, t = mapping.split(',')
+      cht_array << t
+      hash[t] = s
+    end
+
+    [cht_array.join('|'), hash]
+  end
+
+  REGEX_WORDS_T, WORD_T2S = get_words
+  REGEX_PHASES_T, PHASES_T2S = get_phases
 end

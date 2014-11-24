522a20855cc3a17480b11d6d7924da4cff866ef9
wildjcrt <wildjcrt@yahoo.com.tw>
2014-11-24 18:41:34 +0800

    lib/chinese_t2s/middleware, re-write get set_lang and store set_lang in env[rack.session] before @app.call Thanks @godfat 's comment!

--- lib/chinese_t2s/middleware.rb
+++ lib/chinese_t2s/middleware.rb
@@ -6,24 +6,24 @@ module ChineseT2s
       end
 
       def call(env)
+        set_lang = env['QUERY_STRING'][/\bset_lang=(\w+)/, 1]
+
+        unless set_lang.nil?
+          env['rack.session'].delete('chinese_t2s_lang') if set_lang == 'tw'
+          env['rack.session']['chinese_t2s_lang'] = 'cn' if set_lang == 'cn'
+        end
+
         status, headers, bodies = @app.call(env)
 
         if status == 200
           params = env['rack.request.query_hash']
 
-          case bodies
-          when ActionDispatch::Response # Rails
-            if params[:set_lang].present?
-              env['rack.session'].delete(:chinese_t2s_lang) if params[:set_lang] == 'tw'
-              env['rack.session'][:chinese_t2s_lang] = 'cn' if params[:set_lang] == 'cn'
-            end
-
-            if (env['rack.session'][:chinese_t2s_lang] || params[:lang]) == 'cn'
+          if (env['rack.session']['chinese_t2s_lang'] || params['lang']) == 'cn'
+            case bodies
+            when ActionDispatch::Response # Rails
               body = ChineseT2s::translate(bodies.body)
               bodies.body = body
-            end
-          when Rack::BodyProxy # Grape
-            if (env['rack.session'][:chinese_t2s_lang] || params[:lang]) == 'cn'
+            when Rack::BodyProxy # Grape
               body = ChineseT2s::translate(bodies.body.first)
               bodies.body = [body]
             end

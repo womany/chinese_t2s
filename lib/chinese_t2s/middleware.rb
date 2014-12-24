f43d1ac8aa92efa6f783b7edc981aefed88fe9fa
wildjcrt <wildjcrt@yahoo.com.tw>
2014-12-24 16:58:09 +0800

    lib/chinese_t2s/middleware, decode Grpae API JSON string if Rails version is lower than 4.1

--- lib/chinese_t2s/middleware.rb
+++ lib/chinese_t2s/middleware.rb
@@ -24,8 +24,13 @@ module ChineseT2s
               body = ChineseT2s::translate(bodies.body)
               bodies.body = body
             when Rack::BodyProxy # Grape
-              body = ChineseT2s::translate(bodies.body.first)
-              bodies.body = [body]
+              if defined?(::Rails) && (::Rails::VERSION::MAJOR <= 3) || (::Rails::VERSION::MAJOR == 4 && ::RAILS::VERSION::MINOR == 0)
+                body = bodies.body.first
+                body.gsub!(/\\u([0-9a-z]{4})/) { [$1.to_i(16)].pack("U") }
+                body = ChineseT2s::translate(body)
+                bodies.body = [body]
+                headers['Content-Length'] = body.bytesize.to_s
+              end
             when Sprockets::ProcessedAsset
             end
           end
